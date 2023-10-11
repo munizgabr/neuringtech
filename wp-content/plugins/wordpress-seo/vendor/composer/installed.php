91f04c1de722a37a8270cfc34b38fc4596d82d7a
Gabriel Muniz <gabrielrrtolkien@gmail.com>
2023-10-11 14:40:15 -0300

    install plugin

---
+++ wp-content/plugins/wordpress-seo/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => '21.3',
+        'version' => '21.3.0.0',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '03773c992f3f2c0773dc0abbf9473b2cf17c983a',
+        'name' => 'yoast/wordpress-seo',
+        'dev' => false,
+    ),
+    'versions' => array(
+        'composer/installers' => array(
+            'pretty_version' => 'v2.2.0',
+            'version' => '2.2.0.0',
+            'type' => 'composer-plugin',
+            'install_path' => __DIR__ . '/./installers',
+            'aliases' => array(),
+            'reference' => 'c29dc4b93137acb82734f672c37e029dfbd95b35',
+            'dev_requirement' => false,
+        ),
+        'yoast/wordpress-seo' => array(
+            'pretty_version' => '21.3',
+            'version' => '21.3.0.0',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '03773c992f3f2c0773dc0abbf9473b2cf17c983a',
+            'dev_requirement' => false,
+        ),
+    ),
+);
